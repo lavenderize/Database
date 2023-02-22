8befeec09b9137a8b802fd4d74f287133e3ca89e
lavenderize <liexquisit@gmail.com>
2023-02-22 15:32:35 +0900

    Jdbc Test 01

---
+++ src/JdbcExOne.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class JdbcExOne {
+    public static void main(String args[]) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/JDBC_CON";
+        String userName = "root";
+        String password = "lavenderize";
+
+        Connection con = DriverManager.getConnection(url, userName, password);
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("select * from users");
+
+        while (rs.next()) {
+            String name = rs.getString("name");
+            System.out.println(name);
+        }
+
+        rs.close();
+        st.close();
+        con.close();
+    }
+}
